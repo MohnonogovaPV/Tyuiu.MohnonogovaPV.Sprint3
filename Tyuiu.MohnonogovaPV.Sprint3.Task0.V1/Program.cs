8e75636090bf88d120d49b28c40648c429ed0bf4
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-27 20:47:50 +0500

    Add project files.

---
+++ Tyuiu.MohnonogovaPV.Sprint3.Task0.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint3.Task0.V1.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint3.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                                 *");
+            Console.WriteLine("* Задание #0                                                                               *");
+            Console.WriteLine("* Вариант #1                                                                              *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу используя цикл for,                                                   *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при X=4.                                        *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int value = 4;
+            int startValue = 1;
+            int stopValue = 15;
+
+            Console.WriteLine("Переменная x = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Сумму ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
