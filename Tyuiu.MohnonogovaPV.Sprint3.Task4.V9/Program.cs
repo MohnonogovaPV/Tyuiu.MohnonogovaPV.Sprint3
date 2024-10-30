752ecbdb03ad52598881f9e7139d03139552010c
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-30 19:08:49 +0500

    Task4

---
+++ Tyuiu.MohnonogovaPV.Sprint3.Task4.V9/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint3.Task4.V9.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint3.Task4.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                                 *");
+            Console.WriteLine("* Задание #4                                                                               *");
+            Console.WriteLine("* Вариант #9                                                                               *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                 *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение функции              *");
+            Console.WriteLine("* y=x/(cos(x) - x).   При х = 0 пропустить значение. Полученные значения перемножать.      *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
