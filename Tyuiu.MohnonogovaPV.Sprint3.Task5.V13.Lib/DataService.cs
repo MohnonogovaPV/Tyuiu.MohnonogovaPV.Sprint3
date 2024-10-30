72d511ebe8dfcc33a8c81064a0521ec2a01bf86c
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-30 22:30:39 +0500

    Task5

--- Tyuiu.MohnonogovaPV.Sprint3.Task5.V13.Lib/DataService.cs
+++ Tyuiu.MohnonogovaPV.Sprint3.Task5.V13.Lib/DataService.cs
@@ -6,12 +6,12 @@ namespace Tyuiu.MohnonogovaPV.Sprint3.Task5.V13.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double sumSeries = 0;
-            int i, k;
+            int i,k;
             for (i = startValue1; i <= stopValue1; i++)
             {
                 for (k = startValue2; k <= stopValue2; k++)
                 {
-                    sumSeries = sumSeries + (Math.Cos(x) + (k / 2));
+                    sumSeries = sumSeries + (Math.Cos(x) + (k / 2.0));
                 }
             }
             return Math.Round(sumSeries, 3);
